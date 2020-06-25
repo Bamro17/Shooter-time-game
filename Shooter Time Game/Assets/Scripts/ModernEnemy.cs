c4303b7f8187a5d068b75d608d1e237fed1f50a7
Bamro17 <batkin384@gmail.com>
2020-06-25 10:22:55 -0500

    Modern Scene
    Added Modern Scene with basic enemy for testing.

---
+++ Shooter Time Game/Assets/Scripts/ModernEnemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModernEnemy : MonoBehaviour
+{
+    public GameObject player;
+    float speed = 4;
+    public float radius = 10;
+    public float innerRadius = 8;
+    private float shotTimer = 0.0f;
+    public GameObject enemyBullet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(player.transform);
+
+        shotTimer += Time.deltaTime;
+        if (shotTimer > 1)
+        {
+            Instantiate(enemyBullet, this.transform.position, this.transform.rotation);
+            shotTimer = 0;
+        }
+        
+        
+    }
+}
